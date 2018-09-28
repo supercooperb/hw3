35b589efdcc413c75af7b0821e8086b361fa58e4
Cooper Berella <43266554+supercooperb@users.noreply.github.com>
2018-09-28 01:30:05 -0700

    Update speedy-lawnmower.js

--- speedy-lawnmower.js
+++ speedy-lawnmower.js
@@ -8,23 +8,22 @@ var h = 10;
 
 function draw() {
   stroke(random(60, 70), 100, 90);
-  line(x, height+10, x + random(10, 10), height-10-h);
+  line(x, height-10, x + random(-10, 10), height-10-random(h));
 
   x = x + 10;
 
   if (x > width) {
     x = random(10);
-    h = h - 3;
+    h = h + 3;
   }
 
-  if (random() > 0.999) {
+  if (random() > 0.95) {
     noStroke();
     fill(255);
     rect(-1, -1, width+2, height-15);
     h = 10;
   }
 
-  // dirt
   noStroke();
   fill(40, 100, 60);
   rect(0, height-10, width, 10);
