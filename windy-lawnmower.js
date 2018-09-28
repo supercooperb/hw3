6c13d5b117b3b68ea133d572b4be3b72584cde2b
Cooper Berella <43266554+supercooperb@users.noreply.github.com>
2018-09-27 23:40:24 -0700

    Create windy-lawnmower.js

---
+++ windy-lawnmower.js
@@ -0,0 +1,32 @@
+function setup() {
+  createCanvas(400, 200);
+  colorMode(HSB, 360, 100, 100, 100);
+}
+
+var x = 0;
+var h = 10; // controls growth speed of grass
+
+function draw() {
+  stroke(random(60, 70), 100, 90);
+  line(x, height-10, x + 30, height-10-random(h));
+
+  x = x + 10;
+
+  // cutting grass
+  if (x > width) {
+    x = random(10);
+    h = h + 3;
+  }
+
+  if (random() > 0.999) {
+    noStroke();
+    fill(255);
+    rect(-1, -1, width+2, height-15);
+    h = 10;
+  }
+
+  // dirt
+  noStroke();
+  fill(40, 100, 60);
+  rect(0, height-10, width, 10);
+}
